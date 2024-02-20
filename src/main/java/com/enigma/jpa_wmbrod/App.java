b4aa07d61a9b6abbcb6748933c645b95b281d784
irfans18 <nforspeed521@gmail.com>
2024-02-20 13:25:31 +0700

    [WIP] Implement repo

--- src/main/java/com/enigma/jpa_wmbrod/App.java
+++ src/main/java/com/enigma/jpa_wmbrod/App.java
@@ -1,12 +1,15 @@
 package com.enigma.jpa_wmbrod;
 
 import com.enigma.jpa_wmbrod.util.JpaUtil;
+import com.enigma.jpa_wmbrod.view.TotalAgregatView;
 import jakarta.persistence.EntityManager;
 
 public class App {
     public static void main(String[] args) {
         EntityManager entityManager = JpaUtil.getEntityManager();
 
+        TotalAgregatView totalAgregatView = new TotalAgregatView(entityManager);
+        totalAgregatView.getTotalTrx();
 
         entityManager.close();
         JpaUtil.shutdown();
--- src/main/java/com/enigma/jpa_wmbrod/view/TotalAgregatView.java
+++ src/main/java/com/enigma/jpa_wmbrod/view/TotalAgregatView.java
@@ -1,2 +1,24 @@
-package com.enigma.jpa_wmbrod.view;public class TotalAgregatView {
+package com.enigma.jpa_wmbrod.view;
+
+import com.enigma.jpa_wmbrod.entity.Bill;
+import com.enigma.jpa_wmbrod.repository.BillRepository;
+import com.enigma.jpa_wmbrod.repository.impl.BillRepositoryImpl;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class TotalAgregatView {
+    private EntityManager em;
+
+    public TotalAgregatView(EntityManager em) {
+        this.em = em;
+    }
+
+    /**
+     *  1. total transaksi di wmb */
+    public void getTotalTrx(){
+        BillRepository repository = new BillRepositoryImpl(em);
+        List<Bill> bills = repository.findAll();
+        System.out.println(bills);
+    }
 }
