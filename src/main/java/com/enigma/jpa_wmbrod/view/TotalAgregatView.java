6a0d8ca179daeaed6b0ebd27aa0cbf7c24a4fe19
irfans18 <nforspeed521@gmail.com>
2024-02-20 14:08:49 +0700

    [FEATURE] getTotalTrx

--- src/main/java/com/enigma/jpa_wmbrod/view/TotalAgregatView.java
+++ src/main/java/com/enigma/jpa_wmbrod/view/TotalAgregatView.java
@@ -1,6 +1,7 @@
 package com.enigma.jpa_wmbrod.view;
 
 import com.enigma.jpa_wmbrod.entity.Bill;
+import com.enigma.jpa_wmbrod.entity.BillDetail;
 import com.enigma.jpa_wmbrod.repository.BillRepository;
 import com.enigma.jpa_wmbrod.repository.impl.BillRepositoryImpl;
 import jakarta.persistence.EntityManager;
@@ -9,6 +10,7 @@ import java.util.List;
 
 public class TotalAgregatView {
     private EntityManager em;
+    private Integer dashCount = 25;
 
     public TotalAgregatView(EntityManager em) {
         this.em = em;
@@ -19,6 +21,10 @@ public class TotalAgregatView {
     public void getTotalTrx(){
         BillRepository repository = new BillRepositoryImpl(em);
         List<Bill> bills = repository.findAll();
-        System.out.println(bills);
+        int totalTrx = bills.size();
+        System.out.printf("-".repeat(dashCount));
+        System.out.println("\nTotal trx : " + totalTrx);
+        System.out.printf("-".repeat(dashCount));
+        System.out.printf("\n");
     }
 }
