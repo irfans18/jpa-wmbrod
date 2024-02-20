aaba23410e4651856b059bf8967024519de88cfb
Alvindo Tri Jatmiko <alvindo56@gmail.com>
2024-02-20 11:19:27 +0700

    [FEATURE] setup persistence.xml

--- src/main/java/com/enigma/jpa_wmbrod/App.java
+++ src/main/java/com/enigma/jpa_wmbrod/App.java
@@ -1,11 +1,18 @@
 package com.enigma.jpa_wmbrod;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class App {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa.wmbrod");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        transaction.commit();
+        entityManager.close();
         entityManagerFactory.close();
     }
 }
